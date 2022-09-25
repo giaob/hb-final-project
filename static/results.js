f11aac5892279b427135c4c46081e197ed0d71c8
Giao Bui <giaob37@gmail.com>
2022-09-24 17:09:38 -0700

    Added feature that allows switching the map direction.

--- static/results.js
+++ static/results.js
@@ -1,3 +1,13 @@
+let defaultState = false
+
 const button = document.querySelector('#switch-directions-button').addEventListener('click', () => {
-    document.querySelector('#map').innerText = <img src="/static/img/town_b_to_town_a.png"></img>
-  });
+    if (defaultState === false) {
+    console.log("click in false")
+    document.querySelector('#map').innerHTML = "<img src='/static/img/town_b_to_town_a.png'></img>"
+    defaultState = !defaultState
+    } else {
+        console.log("click in else")
+        document.querySelector('#map').innerHTML = "<img src='/static/img/town_a_to_town_b.png'></img>"
+        defaultState = !defaultState
+  }});
+  
